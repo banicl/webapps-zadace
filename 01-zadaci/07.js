3bf8e0c1e0d1b095c9ad3cf67c8bee6a76517b18
Leonarda Banić <banic.leonarda2@gmail.com>
2022-10-08 17:13:47 +0200

    Rjeseni zadatak 7

---
+++ 01-zadaci/07.js
@@ -0,0 +1,14 @@
+var x = 23;
+var vk = function (x) {
+    var ispis = 1;
+    if (x >= 0 && x <= 1000) {
+        for (let i = 1; i <= x; i++) {
+            if (i % 7 == 0) {
+                ispis *= i;
+            }
+        }
+        return ispis;
+    }
+};
+
+console.log("Rezultat dobiven množenjem svih višekratnika broja 7 do 1000: " + vk(x));
